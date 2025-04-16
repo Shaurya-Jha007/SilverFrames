83f0e4c862609fa166d7d0116b9417c03829b6c6
Shaurya Jha <jhashaurya222@gmail.com>
2025-04-16 23:37:42 +0530

    Added new dependencies for routing and also added required assets such as images, fonts, etc. for the hero section.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import DefaultPage from "./pages/DefaultPage";
 
 // const apiKey = import.meta.env.VITE_API_KEY;
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<DefaultPage />} />
+      </Routes>
     </>
   );
 }
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,8 @@
+import {
+  faCirclePlay,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+export default function HeroSection() {
+  return <h1>Hero Section</h1>;
+}

47f91962dc536f7eb5903eda81ed5663dc2c85c8
Shaurya Jha <jhashaurya222@gmail.com>
2025-04-24 23:34:05 +0530

    Added linear gradients to the background of slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -32,6 +32,8 @@ export default function Header() {
           {renderData.map((movie, index) => {
             return (
               <div className="h-[75vh]">
+                <div className="absolute inset-0 before:absolute before:inset-0 before:bg-gradient-to-b before:from-black before:to-transparent before:z-1 after:absolute after:inset-0 after:bg-gradient-to-t after:from-black after:to-transparent after:via-gray-950/60 after:z-1"></div>
+                <div className="absolute inset-0 before:absolute before:right-0 rtl:before:left-0 before:top-0 before:bottom-0 before:w-1/6 before:bg-gradient-to-l before:from-black before:to-transparent after:absolute after:left-0 after:top-0 after:bottom-0 after:w-1/6 after:bg-gradient-to-r after:from-black after:to-transparent z-1"></div>
                 <div
                   style={{
                     backgroundImage: `url(${baseUrl}${movie.backdrop_path})`,

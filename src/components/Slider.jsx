b92469dbb2bda18491ab511e1846a348e1999b50
Shaurya Jha <jhashaurya222@gmail.com>
2025-04-24 22:55:11 +0530

    Carousel completely working, also added custom arrows with separate styles.

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchTrendingMovies } from "../../util/fetch";
 import Slider from "react-slick";
+import Arrow from "../../util/Arrow";
 
-export default function header() {
+export default function Header() {
   const baseUrl = "https://image.tmdb.org/t/p/original";
   const { data, isError, error, isPending } = useQuery({
     queryKey: ["trending"],
@@ -11,28 +12,36 @@ export default function header() {
 
   if (data) {
     const settings = {
-      dots: true,
-      infinite: false,
+      dots: false,
+      infinite: true,
+      fade: true,
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
+      autoplay: true,
+      pauseOnHover: true,
+      autoPlaySpeed: 2000,
+      prevArrow: <Arrow />,
+      nextArrow: <Arrow type={true} />,
     };
     const renderData = data.results.slice(0, 10);
-    console.log(renderData.length);
+    console.log(renderData);
     return (
       <header className="h-[75vh] mt-4">
         <Slider {...settings}>
           {renderData.map((movie, index) => {
             return (
-              <div
-                style={{
-                  backgroundImage: `url(${baseUrl}${movie.backdrop_path})`,
-                  backgroundSize: "cover",
-                  backgroundPosition: "center center",
-                }}
-                className="h-full w-full"
-                key={index}
-              ></div>
+              <div className="h-[75vh]">
+                <div
+                  style={{
+                    backgroundImage: `url(${baseUrl}${movie.backdrop_path})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center center",
+                  }}
+                  className="h-full w-full"
+                  key={index}
+                ></div>
+              </div>
             );
           })}
         </Slider>
---
+++ util/Arrow.jsx
@@ -0,0 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+export default function Arrow({ onClick, type }) {
+  return (
+    <div
+      onClick={onClick}
+      className={`right-6 text-2xl z-10 px-9 py-7 rounded-full bg-gray-400/30 absolute hover:bg-green-400 duration-200 cursor-pointer ${
+        type ? "top-1/3" : "top-1/2"
+      }`}
+    >
+      {type ? (
+        <FontAwesomeIcon icon={faChevronRight} />
+      ) : (
+        <FontAwesomeIcon icon={faChevronLeft} />
+      )}
+    </div>
+  );
+}

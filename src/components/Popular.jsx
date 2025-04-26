409b191cb7d4e96db57120c6de41920814e8999e
Shaurya Jha <jhashaurya222@gmail.com>
2025-04-26 23:38:26 +0530

    Resolved the problem of shows visibility and also added major styling to each and every tile, with some minor ones remaining

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -6,7 +6,7 @@ export default function Popular() {
     queryKey: ["popularMoviesAndShows"],
     queryFn: fetchPopularMoviesAndShows,
   });
-  const baseUrl = "https://image.tmdb.org/t/p/w154";
+  const baseUrl = "https://image.tmdb.org/t/p/original";
 
   const settings = {
     dots: false,
@@ -22,20 +22,26 @@ export default function Popular() {
   if (data) {
     console.log(`${baseUrl}${data[0].poster_path}`);
     return (
-      <section className="h-[50vh] text-white mt-16 px-8 font-bold">
+      <section className="text-white mt-24 px-8 font-bold">
         <h2 className="text-3xl">Popular Movies</h2>
         <Slider {...settings}>
           {data.map((show, index) => {
             return (
-              <div
-                key={index}
-                className="h-96 w-48"
-                style={{
-                  backgroundImage: `url(${baseUrl}${show.poster_path})`,
-                  backgroundSize: "cover",
-                  backgroundPosition: "center center",
-                }}
-              ></div>
+              <div className="h-[55vh] w-full mt-8">
+                <div
+                  key={index}
+                  className="h-5/6 w-11/12 rounded-2xl cursor-pointer hover:scale-110 duration-300 overflow-hidden z-10"
+                  style={{
+                    backgroundImage: `url(${baseUrl}${show.poster_path})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center center",
+                  }}
+                >
+                  <p className="mt-3 ml-3 bg-green-400 inline-block text-xl px-3 py-0.5 rounded-lg">
+                    HD
+                  </p>
+                </div>
+              </div>
             );
           })}
         </Slider>
